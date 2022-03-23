cf8db9f3f75ebe4c309fefa3d161e35f68f8243f
zjt9581 <531290034@qq.com>
2022-03-23 19:15:17 +0800

    init code

---
+++ src/main/java/org/example/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package org.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+
+}
